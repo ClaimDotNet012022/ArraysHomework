c2f589223527cf4adf6afafc51cbb3be185686cb
AndTorGuer <atorresguerrero9@gmail.com>
2022-01-18 16:41:29 -0500

    Adding Andres_ArrayHW

--- ArraysHomework/ArraysHomework.cs
+++ ArraysHomework/ArraysHomework.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
-
+using System.Linq;
 namespace ArraysHomework
 {
     [TestClass]
@@ -11,20 +12,29 @@ namespace ArraysHomework
         {
             // Create an array of ints of size 100
             int[] myArray = new int[100];
+            
             Assert.IsTrue(myArray.Length == 100);
 
-            // take the array made above and fill it so these tests pass  
+            // take the array made above and fill it so these tests pass
+
             myArray[0] = 100;
             myArray[50] = 150;
             myArray[68] = 168;
+
             Assert.IsTrue(myArray[0] == 100);
             Assert.IsTrue(myArray[50] == 150);
             Assert.IsTrue(myArray[68] == 168);
 
             // fill all of the values in to make the loop on line 28 pass as is.
             // Do not edit the loop that assert the passing case. Instead, add code above it to make it pass.
-
+            int sum = 0; 
+            for (int i = 0; i < myArray.Length; i++)
+            {        
+                myArray[i] = sum;
+                sum++;
+            }
             // hint you need some form of repetition.
+
             for (int i = 0; i < myArray.Length; i++)        // Don't edit from HERE
             {                                               //
                 myArray[i] = 10;                            //
@@ -32,21 +42,19 @@ namespace ArraysHomework
 
             }
 
-
             // what wrong with this array declaration? Fix it and make it compile
-            string stringArray = new string[5];
+            string[] stringArray = new string[5];
 
             // What about this one? Fix it and make it compile
-            bool[] boolArray = new bool[];
+            bool[] boolArray = new bool[10];
 
             // What about this one? Fix it and make it compile
-            bool[] boolArray2 = new [20];
+            bool[] boolArray2 = new bool[20];
 
             // Take this array and copy its contents into a new array 
             char[] copyMe = { 'a', 'b', 'c' };
             char[] copiedArray = new char[3];
-
-            
+            copiedArray = copyMe;
 
             Assert.IsTrue(copiedArray.Length == copyMe.Length);
             Assert.IsTrue(copiedArray[0] == 'a');
@@ -55,7 +63,8 @@ namespace ArraysHomework
 
             // Swap the last and first value of this array.
             string[] swapMyValues = { "first", "middle", "last" };
-            
+            swapMyValues[0] = "last";
+            swapMyValues[2] = "first";
             Assert.IsTrue(swapMyValues[0] == "last");
             Assert.IsTrue(swapMyValues[2] == "first");
 
@@ -70,14 +79,15 @@ namespace ArraysHomework
             {
                 array1[up] = up;
                 array2[down] = down;
-                
             }
 
             // Your code here: combine array1 and array2 into an array named combined
             // Everything from array1 should be in the first half of combined, and everything
             // from array2 should be in the second half of combined.
-            
-            
+            int[] combined = new int[50];
+
+            combined = array1.Concat(array2).ToArray();
+
             Assert.IsTrue(combined.Length == 50);
             Assert.IsTrue(combined[0] == array1[0]);
             Assert.IsTrue(combined[24] == array1[24]);
@@ -90,34 +100,39 @@ namespace ArraysHomework
         public void Lists()
         {
             // Create a List of ints named my list
+            List<int> myList = new List<int>();
 
             Assert.IsTrue(myList != null); // if you create it right, this will pass
 
             // Don't worry about how this line works. It's not something you need at this point.
             // All it's doing is making sure that you actually created a List of ints (rather than, for example, an array of ints).
-            Assert.IsTrue(myList.GetType() == typeof(List<int>));   
+            Assert.IsTrue(myList.GetType() == typeof(List<int>));
 
             // take the list made above and fill it so these test passes  
+            myList.Add(21);
+            myList.Add(35);
+            myList.Add(78);
 
             Assert.IsTrue(myList[0] == 21);
             Assert.IsTrue(myList[1] == 35);
             Assert.IsTrue(myList[2] == 78);
 
             // what wrong with this list declaration? You can use any type for its content. Fix it and make it compile
-            List<> stringList = List;
+            List<string> stringList = new List<string>();
 
             // What about this one? You can use any type for its content. Fix it and make it compile
-            List<> list1 = List<>();
+            List<int> list1 = new List<int>();
 
             // What about this one? You can use any type for its content. Fix it and make it compile
-            List<> list2 = new;
+            List<float> list2 = new List<float>();
 
             // What about this one? You can use any type for its content. Fix it and make it compile
-            List<char> list3 = new List<int>;
+            List<char> list3 = new List<char>();
 
             // Take this list and copy its contents into a new list 
             List<char> copyMe = new List<char>() { 'a', 'b', 'c' };
-
+            List<char> copiedList = new List<char>();
+            copiedList = copyMe;
 
             Assert.IsTrue(copiedList.Count == copyMe.Count);
             Assert.IsTrue(copiedList[0] == 'a');
@@ -126,6 +141,8 @@ namespace ArraysHomework
 
             // Swap the last and first value of this list.
             List<string> swapMyValues = new List<string> { "first", "middle", "last" };
+            swapMyValues[0] = "last";
+            swapMyValues[2] = "first";
 
             Assert.IsTrue(swapMyValues[0] == "last");
             Assert.IsTrue(swapMyValues[2] == "first");
@@ -146,6 +163,7 @@ namespace ArraysHomework
             }
 
             // Your code here: combine copy1 into copy 2
+            copy2 = copy1.Concat(copy2).ToList();
 
             Assert.IsTrue(copy2.Count == 50);
 
@@ -157,36 +175,46 @@ namespace ArraysHomework
             // BONUS reverse this list
             List<int> reverseMe = new List<int>() { 1, 2, 3, 4, 5 };
 
-            //Assert.IsTrue(reverseMe[0] == 5);
-            //Assert.IsTrue(reverseMe[1] == 4);
-            //Assert.IsTrue(reverseMe[2] == 3);
-            //Assert.IsTrue(reverseMe[3] == 2);
-            //Assert.IsTrue(reverseMe[4] == 1);
+            reverseMe.Reverse();
+
+            Assert.IsTrue(reverseMe[0] == 5);
+            Assert.IsTrue(reverseMe[1] == 4);
+            Assert.IsTrue(reverseMe[2] == 3);
+            Assert.IsTrue(reverseMe[3] == 2);
+            Assert.IsTrue(reverseMe[4] == 1);
 
             // ---------
-            
+
             // BONUS sort this list
             List<int> sortMe = new List<int>() { 14, 3, 33, 2, 12 };
 
-            //Assert.IsTrue(sortMe[0] == 2);
-            //Assert.IsTrue(sortMe[1] == 3);
-            //Assert.IsTrue(sortMe[2] == 12);
-            //Assert.IsTrue(sortMe[3] == 14);
-            //Assert.IsTrue(sortMe[4] == 33);
+            sortMe.Sort();
+
+            Assert.IsTrue(sortMe[0] == 2);
+            Assert.IsTrue(sortMe[1] == 3);
+            Assert.IsTrue(sortMe[2] == 12);
+            Assert.IsTrue(sortMe[3] == 14);
+            Assert.IsTrue(sortMe[4] == 33);
 
             // ----------
 
             // BONUS Convert sortMe to an array
 
+            int[] sortArray = sortMe.ToArray();
+
             // ----------
 
             // BONUS Check if sortMe Contains the value 2.
 
+            sortMe.Contains(2);
+
             // ----------
 
-            // BONUS Remove all of sort mes values
+            // BONUS Remove all of sortMe values
 
-            //Assert.IsTrue(sortMe.Count == 0);
+            sortMe.Clear();
+
+            Assert.IsTrue(sortMe.Count == 0);
 
         }
     }
